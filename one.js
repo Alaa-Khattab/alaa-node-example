46beba7eefb948afac2047a54e4bf9948199eefb
4daliaomry4 <alaakhattab92@gmail.com>
2017-02-19 12:45:55 +0200

    heroku

--- one.js
+++ one.js
@@ -2,9 +2,9 @@ var http = require('http');
 var fs = require('fs');
 var ress = fs.readFileSync(__dirname + '/index.html', 'utf8');
 http.createServer(function(req, res) {
-    	res.end(ress);
+    	res.end("HELLO ALAA AT GSG");
 
     
-}).listen(8080, function() {
+}).listen(process.evn.PORT, function() {
     console.log('Listening on 8080');
 });
