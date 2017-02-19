95849200595e932f3bcaa92bd956d40aa5f14486
4daliaomry4 <alaakhattab92@gmail.com>
2017-02-19 13:18:13 +0200

    edit

--- one.js
+++ one.js
@@ -1,6 +1,4 @@
 var http = require('http');
-var fs = require('fs');
-var ress = fs.readFileSync(__dirname + '/index.html', 'utf8');
 http.createServer(function(req, res) {
     	res.end("HELLO ALAA AT GSG");
 
