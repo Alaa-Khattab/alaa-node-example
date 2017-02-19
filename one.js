c4031fa3d7997f20f0ca132130f4029cab8decf2
4daliaomry4 <alaakhattab92@gmail.com>
2017-02-19 13:34:01 +0200

    add index.html

--- one.js
+++ one.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var fs = require('fs');
+var result = fs.readFileSync(__dirname +"/index.html","utf8" );
 http.createServer(function(req, res) {
-    	res.end("HELLO ALAA AT GSG");
+    	res.end(result);
 
     
 }).listen(process.env.PORT || 8080, function() {
